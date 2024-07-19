7dd8ee8128be2aba53eb25e656bfd20bb32a69a3
enimaos <ing.devov@gmail.com>
2024-07-19 01:34:34 +0100

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""Log parsing"""
-
+"""
+    script that reads stdin line by line and computes metrics
+"""
 
 import sys
 
 if __name__ == "__main__":
-    fileSize = 0
-    count = 0
-
+    filesize, count = 0, 0
     codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    stats = {i: 0 for i in codes}
+    stats = {k: 0 for k in codes}
 
     def print_stats(stats: dict, file_size: int) -> None:
-        print("File size: {:d}".format(fileSize))
+        print("File size: {:d}".format(filesize))
         for k, v in sorted(stats.items()):
             if v:
                 print("{}: {}".format(k, v))
@@ -28,12 +27,12 @@ if __name__ == "__main__":
             except BaseException:
                 pass
             try:
-                fileSize += int(data[-1])
+                filesize += int(data[-1])
             except BaseException:
                 pass
             if count % 10 == 0:
-                print_stats(stats, fileSize)
-        print_stats(stats, fileSize)
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
     except KeyboardInterrupt:
-        print_stats(stats, fileSize)
+        print_stats(stats, filesize)
         raise
